14de723e04435eb679e2938485d51ee95626ba8c
Melissa Laurino <Melissa.Laurino@Stockton.edu>
2024-09-12 11:54:23 -0400

    Add files via upload

---
+++ Week 1 - Reading and Summarizing Data in R/Week1_DataExoploration_YourLastName.R
@@ -0,0 +1,59 @@
+# Your name here 
+# 9/12/2024
+# Data Exploration - Fall 2024
+# Week 1 Practice - Reading and Summarizing Data in R - Melissa Laurino & Dr. Clif Baldwin
+
+# Get your working directory. Your working directory is the path to this file. This may look different for PC users/Mac users.
+
+
+# Set your working directory:
+
+
+# Read in the data from the .csv file:
+# Use TAB to auto complete
+# "file name", - Case and space sensitive!
+# header = TRUE - because our data has headers (Column names)
+
+
+# Check the dimensions of the data
+
+
+
+# Summarize your entire data
+
+
+# View only the first 6 rows of data
+
+
+# List column names within your data
+
+
+# Summarize only a specific column by name using $ in your syntax
+
+
+# View the structure of your data 
+
+
+# Install the package first:
+# Comment your code with a hashtag to avoid running a line a second time. 
+# install.packages("tidyverse") 
+
+
+# Use glimpse() to view the data:
+
+
+# Select a specific column of your data:
+
+
+# Use filter to select for a specific species, truncatus, within the column Species
+
+
+# Install package readxl as recommended by R for Data Science
+# https://r4ds.hadley.nz/spreadsheets
+# install.packages("readxl")
+
+
+#Load the package
+
+
+#Load the data
